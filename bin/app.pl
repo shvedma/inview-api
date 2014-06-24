91c4c349238fcdd89747a64cdd8beabc4abd7684
Maria Shvedova <shvedma@gmail.com>
2014-06-24 17:57:26 +1000

    An attempt to plugin plack authentication. Doesnt work.YET

--- bin/app.pl
+++ bin/app.pl
@@ -1,4 +1,29 @@
 #!/usr/bin/env perl
 use Dancer;
 use Inview::API;
+
+use Plack::Builder;
+
+my $app = sub {
+    my $env     = shift;
+    my $request = Dancer::Request->new($env);
+    Dancer->dance($request);
+};
+
+builder {
+    enable "Auth::Basic", authenticator => sub {
+        # stub to do authentication and authorisation - get cookie etc.
+        # from InviewAuthen and InviewAuthz
+        # makes sense to combine?
+
+        # blah blah and eventually user from we get user id and company id 
+        # my $ret = Oriel::Apache2::InviewSessionCache::get_session( $session );
+        debug "HAHA working";
+        session 'user' => 'blah'; 
+        1;
+        
+    };
+    $app;
+};
+
 dance;
--- lib/Inview/API.pm
+++ lib/Inview/API.pm
@@ -4,7 +4,14 @@ use Dancer ':syntax';
 use Inview::API::Service;
 use Inview::API::Alert;
 
+use Dancer::Plugin::REST;
+
+
 our $VERSION = '0.1';
+
+hook 'before' => sub {
+  return halt(status_forbidden ("Bad user no cookie")) if (! session('user'));
+};
 
 get '/' => sub {
     template 'index';
